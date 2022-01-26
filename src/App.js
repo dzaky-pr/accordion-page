281bc6cba6c008f988b2155a2e2392d5be812fff
Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>
2022-01-27 06:37:28 +0700

    fix #4
    Signed-off-by: Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [question, setQuestion] = useState(data);
+
+  return (
+    <main>
+      <div className="container">
+        <h3>question and answer about login</h3>
+        <section className="info">
+          {question.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
